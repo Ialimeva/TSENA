c8089af78d1d4203ad9881a28aaeb7d3561dd54c
Ialimeva <mevashy@gmail.com>
2025-04-23 22:32:44 +0300

    Create admin form

--- users/forms.py
+++ users/forms.py
@@ -1,1 +1,42 @@
 from django import forms
+from users.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class AdminRegistration(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+    # Custom labels and error messages for the for fields
+    def __init__(self, *args, **kwargs):
+        # Create a copy of UserCreationForm by calling it with super() and putting that copy inside of Admin register form
+        super().__init__(*args, **kwargs)
+
+
+        # Now we have our own copy of UserCreationForm that we can modify to our liking and use it in AdminRegistration
+        self.fields['first_name'].label = 'Nom'
+        self.fields['first_name'].error_messages = {
+            'required': 'Le champ nom est obligatoire',
+        }
+        self.fields['last_name'].label = 'Prénom'
+        self.fields['last_name'].error_messages = {
+            'required': 'Le champ prénom est obligatoire',
+        }
+        self.fields['username'].label = 'Nom d\'utilisateur'
+        self.fields['username'].error_messages = {
+            'required': 'Le champ nom d\'utilisateur est obligatoire',
+            'unique': 'Le nom d\'utilisateur n\'est plus disponible',
+        }
+        self.fields['email'].label = 'Email'
+        self.fields['email'].error_messages = {
+            'required': 'Le champ email est obligatoire',
+            'unique': 'L\'adresse email est déja associé à un compte',
+        }
+        self.fields['password1'].label = 'Mot de passe'
+        self.fields['password1'].error_messages = {
+            'required': 'Le champ mot de passe est obligatoire',
+        }
+        self.fields['password2'].label = 'Comfirmez le mot de passe'
+        self.fields['password2'].error_messages = {
+            'required': 'Le champ comfirmation est obligatoire',
+        }
